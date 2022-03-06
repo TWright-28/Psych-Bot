6657732603ad0e4334aa695c874c0ffd41d60323
Dima Zhuravel <dmitrozhyravel@gmail.com>
2022-03-05 19:39:16 -0800

    implementation of GUI

--- bot.py
+++ bot.py
@@ -4,14 +4,13 @@ from fileReader import FileReader
 
 class Bot:
 	"""
-		Constructor to initialize constant class variables, and start the program.
+		Constructor to initialize constant class variables, and setup the responses.
 	"""
 	def __init__(self):
+		self.name = ""
 		self.stemmer = PorterStemmer()
 		self.conditions = {}
 		self.initialize()
-		self.setUserName()
-		self.initializeChat()
 
 	"""
 		Using the file reader to read in the data. using the stemmer class to check if the key matches the condition of the word.
@@ -40,63 +39,48 @@ class Bot:
 		If the program receives an empty response, user will be prompted to enter the answer again.
 		The response to the second question is saved to the node, which will be used in initializeChat method.
 	"""
-	def setUserName(self):
-		self.name = input("Hello, I am Psych-Bot. What is your name? ")
-		
-		while len(self.name) == 0:
-			self.name = input("I didn't get your name, please, repeat.\n- ")
-
-		response = input(f"Hello {self.name}.\nI am glad to have you here today, How are you feeling?\n- ")
-
-		while len(response) == 0:
-			response = input("Sorry, what did you say?\n- ")
-
+	def setUserName(self, name):
+		self.name = name
 		self.current = self.nodes[0]
-	
+		return f"> Bot: Hello {self.name}.\nI am glad to have you here today, How are you feeling?\n\n"
+
 	"""
 		Return the user's name.
 	"""
 	def getUserName(self):
+		if len(self.name) == 0:
+			return -1
 		return self.name
  
 	"""
+		TODO: Rewrite the purpose of the method.
 		This runs the main chat loop, exits when the next node is none.
 		If the user inputs keyword "quit," program will exit.
 		If the user inputs an empty input, program will prompt to enter correct response.
 	"""
-	def initializeChat(self):
+	def getResponse(self, answer):
 		nodeValue = self.current
 		
-		while nodeValue != None:
-			if 'print' in nodeValue:
-				print(nodeValue['text'])
-				nodeValue = self.findNode(nodeValue['children'][0])
-				continue
-			
-			answer = input(f"{nodeValue['text']}\n- ")
-			
-			while len(answer) == 0:
-				answer = input("Sorry, what did you say?\n- ")
+		# while nodeValue != None:
+		if 'print' in nodeValue:
+			print(nodeValue['text'])
+			nodeValue = self.findNode(nodeValue['children'][0])
 
-			if answer.lower() == "quit":
-				print("Thank you for your questions. Have a nice day!")
-				break
-
-			if len(nodeValue['children']) == 1:
-				nodeValue = self.findNode(nodeValue['children'][0])
-			else:
-				answer_words = [self.stemmer.stem(word) for word in answer.lower().split(" ")]
-				for child in nodeValue['children']:
-					child = self.findNode(child)
-					if 'default' in child:
+		if len(nodeValue['children']) == 1:
+			nodeValue = self.findNode(nodeValue['children'][0])
+		else:
+			answer_words = [self.stemmer.stem(word) for word in answer.lower().split(" ")]
+			for child in nodeValue['children']:
+				child = self.findNode(child)
+				if 'default' in child:
+					nodeValue = child
+					break
+				for word in self.conditions[child['condition']]:
+					if word in answer_words:
 						nodeValue = child
-						break
-					for word in self.conditions[child['condition']]:
-						if word in answer_words:
-							nodeValue = child
-					if nodeValue == child:
-						break
-
+				if nodeValue == child:
+					break
+		return nodeValue['text']
 """
 	Runs the chat with the command 'python bot.py'.
 """
